b1f35ab0ad830de477b50cd24f94b9e27ec48334
Ronnie Hodges <rankmehighest@gmail.com>
2021-12-20 12:11:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,29 @@ let missed = 0;
 const phrases = ['Micheal Jordan', 'Wayne Gretzky', 'Muhamad Ali', 'Bill Belichick', 'Jackie Robinson'];
 
 startGame[0].addEventListener('click', () => {
+
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+
 //This function retreives a Random phrase from an array
 function getRandomPhraseAsArray(arr) {
 
-    const arrayLength = arr.length;
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+function addPhraseToDisplay(arr) {
+
+    for( const i = 0; i < arr.length; i++ ) {
+
+    }    
+
+    const element = document.createElement('li');
+    element.textContent = 'arr[i]';
+    phrase.appendChild(li);
+
+    // if( phrase. ) {
+
+    // }
 }
